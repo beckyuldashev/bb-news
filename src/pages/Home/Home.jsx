32b6e535869846582a792c21d38a47c386e0aeb1
Beck Yuldashev <yuldashev.bek@gmail.com>
2023-12-10 16:24:05 +0300

    add pagination, change api

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import NewsBanner from "../../components/NewsBanner/NewsBanner";
 import NewsList from "../../components/NewsList/NewsList";
 import Skeleton from "../../components/Skeleton/Skeleton";
+import Pagination from "../../components/Pagination/Pagination";
 
 import { getNews } from "../../api/apiNews";
 
@@ -11,11 +12,14 @@ import cl from "./Home.module.css";
 const Home = () => {
   const [news, setNews] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = 10;
+  const pageSize = 10;
 
   const fetchNews = async () => {
     try {
       setIsLoading(true);
-      const data = await getNews();
+      const data = await getNews(currentPage, pageSize);
       setNews(data.news);
     } catch (error) {
       console.log(error);
@@ -26,7 +30,7 @@ const Home = () => {
 
   useEffect(() => {
     fetchNews();
-  }, []);
+  }, [currentPage]);
 
   return (
     <div className={cl.home}>
@@ -36,11 +40,25 @@ const Home = () => {
         <Skeleton count={1} type="banner" />
       )}
 
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        setCurrentPage={setCurrentPage}
+        isLoading={isLoading}
+      />
+
       {news.length > 0 && !isLoading ? (
         <NewsList news={news} />
       ) : (
         <Skeleton count={6} type="item" />
       )}
+
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        setCurrentPage={setCurrentPage}
+        isLoading={isLoading}
+      />
     </div>
   );
 };

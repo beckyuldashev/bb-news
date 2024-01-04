3cb578fd6d1e12bd78b925a412e1d996c67ef999
Beck Yuldashev <yuldashev.bek@gmail.com>
2024-01-04 21:27:06 +0300

    fix context error

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.jsx";
-import { ThemeProvider } from "./context/themeContext.js";
+import { ThemeProvider } from "./context/ThemeContext.js";
 
 import "./index.css";
 
